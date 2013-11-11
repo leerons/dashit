3034cd54540c745e18c70331321a602df3319076
Leeron Shalev <23leeron23@gmail.com>
2013-11-11 15:46:22 +0200

    testing gpio

--- pi/main.js
+++ pi/main.js
@@ -1,5 +1,15 @@
 var gpio = require("gpio");
 
 var port_17 = gpio.export(17, {interval: 400, ready: function(){
-    port_17.set();
+    ledOn();
 }});
+
+function ledOn(){
+    port_17.set();
+    setTimeOut(ledOff, 1000);
+}
+
+function ledOff(){
+    port_17.reset();
+    setTimeOut(ledOn, 1000);
+}
