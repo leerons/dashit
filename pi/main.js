cf94d1b5ade4d1e4f640d8ff85e260e194db6f5e
Leeron Shalev <23leeron23@gmail.com>
2013-11-11 16:37:48 +0200

    testing gpio

--- pi/main.js
+++ pi/main.js
@@ -1,8 +1,15 @@
 var gpio = require("gpio");
 
-var port_17 = gpio.export(17, {interval: 400, ready: function(){
-    ledOn();
-}});
+var LED_PORT = 17, SWITCH_PORT = 25;
+
+var ledPort = gpio.export(LED_PORT, {direction: 'out'});
+var switchPort = gpio.export(SWITCH_PORT, {direction: 'in', interval:400});
+
+switchPort.on('change', function(val){
+    console.log("switch value: " + val);
+    ledPort.set(val);
+});
+
 
 function ledOn(){
     port_17.set();
