a7d18afdfbd823a5cbfaab726466b8ba6cb5db21
Leeron Shalev <23leeron23@gmail.com>
2013-11-12 12:29:55 +0200

    Testing DDP on Pi

--- pi/main.js
+++ pi/main.js
@@ -8,15 +8,32 @@ var switchPort = gpio.export(SWITCH_PORT, {direction: 'in', interval:400});
 switchPort.on('change', function(val){
     console.log("switch value: " + val);
     ledPort.set(val);
+    ddpclient.call('toiletRoomStateChange', {"value": val}, function(err, result) {
+        console.log('called toiletRoomStateChange on server, result: ' + result);
+    });
 });
 
 
-function ledOn(){
-    port_17.set();
-    setTimeout(ledOff, 1000);
-}
 
-function ledOff(){
-    port_17.reset();
-    setTimeout(ledOn, 1000);
-}
+
+var DDPClient = require("ddp");
+
+var ddpclient = new DDPClient({
+  host: "192.168.28.15", 
+  port: 3000,
+  /* optional: */
+  auto_reconnect: true,
+  auto_reconnect_timer: 500,
+  use_ejson: true,  // default is false
+  use_ssl: false, //connect to SSL server,
+  use_ssl_strict: true //Set to false if you have root ca trouble.
+});
+
+ddpclient.connect(function(error) {
+  if (error) {
+    console.log('DDP connection error!');
+    return;
+  }
+
+  console.log('connected!');
+});
