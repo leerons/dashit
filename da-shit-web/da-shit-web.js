699889e4170afaa05d85725af7ec5ed07467a057
Leeron Shalev <23leeron23@gmail.com>
2013-11-17 02:27:12 +0200

    Will it work? 2:30 am ...

--- da-shit-web/da-shit-web.js
+++ da-shit-web/da-shit-web.js
@@ -5,23 +5,117 @@ BathroomsState = new Meteor.Collection("bathrooms_state");
 
 // Client
 if (Meteor.isClient) {
+  $( document ).ready(function() {
+    resizeLayout();    
+    $( window ).resize(resizeLayout);
+  });
+
+  function resizeLayout(){
+    var body = $("body");
+    var bodyHeight = body.height();
+    var bodyWidth = body.width();
+
+    // Content div
+    var contentDiv = $("#content");
+    var contentDivWidth = bodyHeight * 0.8333;
+    var contentDivMarginLeft = (bodyWidth - contentDivWidth) / 2;
+    contentDiv.width(contentDivWidth);
+    contentDiv.css("margin-left", contentDivMarginLeft);
+
+    bathroomsLayout(bodyHeight, bodyWidth);
+
+    
+  }
+
+  function messageLayout(){
+    var messageTop = $("#floor").position().top * 1.15;
+    $("#message").css("top", messageTop)
+  }
+
+  function bathroomsLayout(bodyHeight, bodyWidth){
+    if(bodyHeight == null){
+      var body = $("body");
+      var bodyHeight = body.height();
+      var bodyWidth = body.width();
+    }
+
+    
+    var doorHeight = bodyHeight * 0.4;
+    var doorWidth = doorHeight * 0.496;
+    var doorsGap = bodyWidth * 0.1; 
+
+    // Indicator lights
+    var indicatorLightWidth = doorWidth * 0.163;
+    var indicatorLightLeft = (doorWidth - indicatorLightWidth) / 2;
+    var indicatorLightTop = indicatorLightWidth * 1.166;
+    $(".indicator-light").height(indicatorLightWidth).width(indicatorLightWidth)
+    .css({"left": indicatorLightLeft}).css({"top": - indicatorLightTop});
+
+    // Doors
+    $(".door").height(doorHeight).width(doorWidth);
+
+    // Bathrooms
+    for(var i = 0; i < 3; i++){
+      var left = i * (doorWidth + doorsGap);
+      $("#room_"+i).css("left",left)
+    }
+
+    var bathroomsBottom = $("#floor").position().top - 3;
+    var bathroomsDiv = $("#bathrooms");
+    bathroomsDiv.css("bottom", bathroomsBottom);
+  }
+
   Meteor.subscribe("bathroomsCurrentState");
 
-  Template.comming_soon.description = function () {
-    return "DaShit utilizes the shit-E-zx-6000, a handcrafted bathroom door sensor. The door sensors are connected to a Raspberry Pi controller and updates a MongoDB + Node.js + Meteor.js web server. The daShit website is kept up-to-date using web socket technology for a real time Know if You Can Go (TM) information at all times.";
-  };
-
-  Template.comming_soon.rooms = function () {
-    var roomsStr = "";
+  Template.bathrooms.rooms = function(){
     var state = BathroomsState.findOne({floor: currentFloor});
-    if(state){
-      roomsStr = "Floor #" + state.floor + " | rooms: [";
-      for(var i = 0; i < state.rooms.length; i++){
-        roomsStr += state.rooms[i] + ", ";
+    console.log("rooms: "+state.rooms);
+    var roomsArray = [];
+    for(var i = 0; i < state.rooms.length; i++){
+      roomsArray[i] = { "index": i,
+                        "indicatorState": (state.rooms[i] == OPEN) ? "green" : "red",
+                        "doorState": state.rooms[i]};
+    }
+    return roomsArray;
+  }
+
+  function openRooms(){
+    var open = 0;
+    var rooms = BathroomsState.findOne({floor: currentFloor}).rooms;
+    for(var i = 0; i < rooms.length; i++){
+      if(rooms[i] == OPEN){
+        open++;
       }
     }
-    return roomsStr;
-  };
+    return open;
+  }
+
+  Template.message.title = function(){
+    if(openRooms() > 0){
+      return "woohoo";
+    } else {
+      return "oops";
+    }
+  }
+
+  Template.message.content = function(){
+    if(openRooms() > 0){
+      return "bla";
+    } else {
+      return "ggg";
+    }
+  }
+
+  Template.message.graphics = function(){
+    if(openRooms() > 0){
+      return "mr-turd";
+    } else {
+      return "shit-in-a-jar";
+    }
+  }
+
+  Template.bathrooms.rendered = bathroomsLayout;
+  Template.message.rendered = messageLayout;
 }
 
 
@@ -29,7 +123,9 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    BathroomsState.upsert({floor:2}, {floor:2, rooms: [CLOSE, CLOSE, CLOSE]});
+    if(BathroomsState.find({floor:currentFloor}).count == 0){
+      BathroomsState.insert({floor:currentFloor, rooms: [CLOSE, CLOSE, CLOSE]});  
+    }   
   });
 
   Meteor.publish("bathroomsCurrentState", function(){
--- pi/main.js
+++ pi/main.js
@@ -1,25 +1,7 @@
-var gpio = require("gpio");
-
-var LED_PORT = 17, SWITCH_PORT = 27;
-
-var ledPort = gpio.export(LED_PORT, {direction: 'out'});
-var switchPort = gpio.export(SWITCH_PORT, {direction: 'in', interval:400});
-
-switchPort.on('change', function(val){
-    console.log("switch value: " + val);
-    ledPort.set(val);
-    ddpclient.call('toiletRoomStateChange', [val], function(err, result) {
-        console.log('server result: ' + result);
-    });
-});
-
-
-
-
 var DDPClient = require("ddp");
 
 var ddpclient = new DDPClient({
-  host: "192.168.28.15", 
+  host: WEB_SERVER_URL, 
   port: 3000,
   auto_reconnect: true,
   auto_reconnect_timer: 500,
@@ -27,9 +9,63 @@ var ddpclient = new DDPClient({
 
 ddpclient.connect(function(error) {
   if (error) {
-    console.log('DDP connection error!');
+    console.log('DDP server connection error!\n' + JSON.stringify(error));
     return;
   }
 
-  console.log('connected!');
+  console.log('Connected to Meteor server at '+WEB_SERVER_URL);
 });
+
+
+var gpio = require("gpio");
+var async = require"async");
+var WEB_SERVER_URL = "192.168.28.15";
+
+// Bathroom microswitch ports
+var BATHROOM_PORTS = [{"index": 0, "portID": 2}, 
+                      {"index": 1, "portID": 3}, 
+                      {"index": 2, "portID": 4}];
+
+var currentSate = [];
+var pendingPorts = BATHROOM_PORTS.length; 
+
+// Export ports as inputs
+var bathroomInputs = [];
+
+
+async.each(BATHROOM_PORTS, function(item, callback){
+  var i = item.index;
+  var port = item.portID;
+  // Initialize
+  var input = bathroomInputs[i] = gpio.export(port, {direction: 'in', interval:200, 
+    ready: function() {
+      currentSate[i] = input.value;
+      console.log("Initial value for input #" + i + ": " + value);
+      sendStateUpdateToServer(i, value);
+      callback();
+    }
+  });
+  // Change event handler 
+  input.on('change', function(value){
+    console.log("Input #" + i + " value changed | new value: " + value);
+    sendStateUpdateToServer(i, value);
+  });
+},
+function(error){
+  // Nothing here at the moment
+});
+
+function sendStateUpdateToServer(index, value){
+  var floor = 2; // Hard coded for the time being
+  var room = index;
+  var state = value ? "open" : "close";
+  var data = {"floor": floor, "room": room, "state": state};
+  console.log("Sending server update: " + JSON.stingify(data));
+  ddpclient.call('toiletRoomStateChange', [data], function(err, result) {
+      if(err){
+        console.log("Server error: "+ err);
+      } else {
+        console.log('Server result: ' + result);
+      }
+  });
+}
