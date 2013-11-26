37bc5bd9dfc85bc24f60581fb6873f2d8aa0221b
Leeron Shalev <23leeron23@gmail.com>
2013-11-26 10:06:58 +0200

    Avoid from using fractions of pixel

--- web/main.js
+++ web/main.js
@@ -20,31 +20,31 @@ if (Meteor.isClient) {
     // Content div
     var contentDiv = $("#content");
     if(bodyWidth > bodyHeight * 0.6){
-      var contentDivWidth = bodyHeight * 0.85;
-      var contentDivHeight =  bodyHeight * 0.7;
-      var contentDivMarginLeft = (bodyWidth - contentDivWidth) / 2;
+      var contentDivWidth = Math.round(bodyHeight * 0.85);
+      var contentDivHeight =  Math.round(bodyHeight * 0.7);
+      var contentDivMarginLeft = Math.round((bodyWidth - contentDivWidth) / 2);
       contentDiv.height(contentDivHeight).width(contentDivWidth)
         .css("left", contentDivMarginLeft);
     } else {
       var contentDivWidth = bodyWidth;
-      var contentDivHeight = bodyWidth * 1.18;
+      var contentDivHeight = Math.round(bodyWidth * 1.18);
       contentDiv.height(contentDivHeight).width(contentDivWidth).css("left", 0);  
     }
     $("#floor").css("top", contentDivHeight);
 
     // Logo
     var logoDiv = $("#logo");
-    var logoDivHeight = contentDivHeight * 0.22;
-    var logoDivWidth = logoDivHeight * 2.8;
-    var logoLeft = (contentDivWidth - logoDivWidth) / 2;
-    var logoDivTop = contentDivHeight * 0.05;
+    var logoDivHeight = Math.round(contentDivHeight * 0.22);
+    var logoDivWidth = Math.round(logoDivHeight * 2.8);
+    var logoLeft = Math.round((contentDivWidth - logoDivWidth) / 2);
+    var logoDivTop = Math.round(contentDivHeight * 0.05);
     logoDiv.width(logoDivWidth).height(logoDivHeight).css({"left": logoLeft, "top": logoDivTop});
 
     bathroomsLayout(contentDiv);
   }
 
   function messageLayout(){
-    var messageTop = $("#floor").position().top * 1.15;
+    var messageTop = Math.round($("#floor").position().top * 1.15);
     $("#message").css("top", messageTop);
   }
 
@@ -54,18 +54,18 @@ if (Meteor.isClient) {
     var parentWidth = parent.width();
     var parentPosition = parent.position();
 
-    var doorHeight = parentHeight * 0.57;
-    var doorWidth = doorHeight * 0.496;
-    var doorsGap = parentWidth * 0.1; 
+    var doorHeight = Math.round(parentHeight * 0.57);
+    var doorWidth = Math.round(doorHeight * 0.496);
+    var doorsGap = Math.round(parentWidth * 0.1); 
 
     // Indicator lights
-    var indicatorLightWidth = doorWidth * 0.163;
-    var indicatorLightLeft = (doorWidth - indicatorLightWidth) / 2;
+    var indicatorLightWidth = Math.round(doorWidth * 0.163);
+    var indicatorLightLeft = Math.round((doorWidth - indicatorLightWidth) / 2);
     $(".indicator-light").height(indicatorLightWidth).width(indicatorLightWidth)
       .css({"left": indicatorLightLeft});
 
     // Doors
-    var doorsTop = indicatorLightWidth * 1.166;
+    var doorsTop = Math.round(indicatorLightWidth * 1.166);
     $(".door").css("top", doorsTop).height(doorHeight).width(doorWidth);
     var doorsCount = 3;
 
@@ -75,8 +75,8 @@ if (Meteor.isClient) {
       $("#room_"+i).css("left",left);
     }
 
-    var doorsWidth = doorsCount * doorWidth + (doorsCount - 1 ) * doorsGap;
-    var doorsStartLeft = (parentWidth - doorsWidth) / 2;
+    var doorsWidth = Math.round(doorsCount * doorWidth + (doorsCount - 1 ) * doorsGap);
+    var doorsStartLeft = Math.round((parentWidth - doorsWidth) / 2);
     $("#bathrooms").height(doorsTop + doorHeight).width(doorsWidth).css("left", doorsStartLeft); 
   }
 
